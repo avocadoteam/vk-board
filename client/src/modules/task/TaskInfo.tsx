d2fe5bd8d687cf5e0cfd4fe7fc22933f995c5cc8
konst <konstantin.mikheev@icord.cz>
2020-08-20 13:07:11 +0200

    time info

--- client/src/modules/task/TaskInfo.tsx
+++ client/src/modules/task/TaskInfo.tsx
@@ -3,9 +3,12 @@ import { useFela } from 'react-fela';
 import Icon20ArticleOutline from '@vkontakte/icons/dist/20/article_outline';
 import Icon20RecentOutline from '@vkontakte/icons/dist/20/recent_outline';
 import { MembershipItem } from 'core/models';
-import { UsersStack } from '@vkontakte/vkui';
+import { UsersStack, Text } from '@vkontakte/vkui';
 import { useSelector } from 'react-redux';
 import { isThemeDrak } from 'core/selectors/common';
+import { ru } from 'date-fns/locale';
+import { format } from 'date-fns';
+
 type Props = {
   dueDate: string | null;
   memberships: MembershipItem[];
@@ -24,7 +27,7 @@ export const TaskInfo = React.memo<Props>(({ dueDate, memberships = [] }) => {
       })}
     >
       <Icon20ArticleOutline className={css(iconStyle)} />
-      {dueDate !== null && <Icon20RecentOutline className={css(iconStyle)} />}
+      <TimeInfo dueDate={dueDate} />
       {memberships?.length > 1 ? (
         <UsersStack
           photos={memberships.map((m) => m.avatar)}
@@ -44,4 +47,29 @@ const iconStyle = () => ({
   width: '22px !important',
   height: '22px !important',
   '>svg': { width: '22px', height: '22px' },
+});
+
+const TimeInfo = React.memo<Pick<Props, 'dueDate'>>(({ dueDate }) => {
+  const { css } = useFela();
+  if (dueDate === null) {
+    return null;
+  }
+
+  return (
+    <>
+      <Icon20RecentOutline className={css(iconStyle)} />
+      <Text
+        weight="semibold"
+        className={`useMonrope ${css({
+          fontSize: '13px',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          alignSelf: 'center',
+        })}`}
+      >
+        {format(new Date(dueDate), 'dd MMMM', { locale: ru })}
+      </Text>
+    </>
+  );
 });
