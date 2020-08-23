7459056e1a16577ff6894f674ac3f09e82388aba
konst <konstantin.mikheev@icord.cz>
2020-08-23 18:14:23 +0200

    build

--- server/public/precache-manifest.68c7e11b66f80c5363bd0855e9547b33.js
+++ server/public/precache-manifest.68c7e11b66f80c5363bd0855e9547b33.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bbacd20e3940e86b87a5f360700dc9f1",
+    "revision": "afe053d1a3256154ed2eb93509a8d315",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.1e562f28.chunk.css"
   },
   {
-    "revision": "f37bf5c3bbf38566330d",
+    "revision": "4f1899884ff797d97b47",
     "url": "/static/css/main.f0121168.chunk.css"
   },
   {
@@ -32,8 +32,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.0df4f4d3.chunk.js"
   },
   {
-    "revision": "f37bf5c3bbf38566330d",
-    "url": "/static/js/main.0987b3b5.chunk.js"
+    "revision": "4f1899884ff797d97b47",
+    "url": "/static/js/main.8726767b.chunk.js"
   },
   {
     "revision": "9b6ed41d845ef97c9363",
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.171f730c774396d9c909009dc6c4ebcb.js"
+  "/precache-manifest.68c7e11b66f80c5363bd0855e9547b33.js"
 );
 
 self.addEventListener('message', (event) => {
