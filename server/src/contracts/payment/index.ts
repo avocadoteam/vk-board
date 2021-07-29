914b65158a8ccb2d5fc6574319101b9249945b9a
konst <konstantin.mikheev@icord.cz>
2021-07-29 08:31:10 +0200

    allow all

--- server/src/contracts/payment/index.ts
+++ server/src/contracts/payment/index.ts
@@ -1,3 +1,5 @@
+import { Allow } from 'class-validator';
+
 export enum NotificationType {
   GetItem = 'get_item', //— получение информации о товаре;
   GetItemTest = 'get_item_test', //— получение информации о товаре;
@@ -8,26 +10,36 @@ export enum NotificationType {
 }
 
 export class PaymentVoice {
+  @Allow()
   notification_type: NotificationType;
-
+  @Allow()
   app_id: number;
+  @Allow()
   user_id: number;
+  @Allow()
   receiver_id: number;
-
+  @Allow()
   order_id: number;
-
+  @Allow()
   subscription_id: number;
-
+  @Allow()
   version: string;
-
+  @Allow()
   sig: string;
-
+  @Allow()
   date: number;
+  @Allow()
   status: 'chargeable' | 'refunded';
+  @Allow()
   item: string;
+  @Allow()
   item_id: number;
+  @Allow()
   item_title: string;
+  @Allow()
   item_photo_url: string;
+  @Allow()
   item_price: string;
+  @Allow()
   item_discount: string;
 }
